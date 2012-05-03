24cf6aefa08d58d525f2077040371f65a128ae14
Jason Hall <imjasonh@gmail.com>
2012-05-02 21:14:05 -0400

    Update redirect URL

--- sample.dart
+++ sample.dart
@@ -3,7 +3,7 @@
 
 String _CLIENT_ID = 'T0JY25PVRECQGBURYCFBC4NHWOQDFFFHA11YRHTQEGF5E00I';
 String _REDIRECT_URI =
-    'http://127.0.0.1:3030/Users/jasonhall/git/foursquare-dart/oauthWindow.html';
+    'http://foursquare-dart.googlecode.com/git/oauthWindow.html';
 
 main() {
   Foursquare fsq = new Foursquare(_CLIENT_ID);
@@ -15,9 +15,10 @@ main() {
       fsq.users.get().execute().then((userResp) {
         window.alert('Hello ${userResp["response"]["user"]["firstName"]}');
 
-        fsq.requestFactory.build('GET', 'updates/notifications').execute().then((notificationsResp) {
-          window.alert('Notifications: ${notificationsResp["response"]["notifications"]["items"].length}');
-        });
+        fsq.requestFactory.build('GET', 'updates/notifications').execute()
+            .then((notificationsResp) {
+              window.alert('Notifications: ${notificationsResp["response"]["notifications"]["items"].length}');
+            });
       });
     });
   });
