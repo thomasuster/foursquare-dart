5f2f2980790720bb27c6edc2444448dde1199686
Jason Hall <imjasonh@gmail.com>
2012-05-03 03:11:00 -0400

    Fix up sample slightly

--- sample.dart
+++ sample.dart
@@ -12,21 +12,29 @@ main() {
   e.on.click.add((_) {
     fsq.login(REDIRECT_URI).then((__) {
       e.remove();
+
+      // Example of a standard request, for the current user's information.
       fsq.users.get().execute().then((r1) {
-        window.alert('Hello ${r1["response"]["user"]["firstName"]}');
-
-        // Example of building an arbitrary request not covered by a
-        // pre-defined utility methods.
-        new Request('GET', 'updates/notifications').execute().then((Map r2) {
-          window.console.log(r2['response']['notifications']['count']);
-        });
-
-        // Example of making a multi request
-        fsq.multi([fsq.users.get(), fsq.users.get('32'), fsq.users.get('33')])
-            .execute().then((Map r3) {
-              window.console.log(r3['response']['responses'].length);
-            });
+        log('Hello ${r1["response"]["user"]["firstName"]}');
       });
+
+      // Example of building an arbitrary request not covered by a pre-defined
+      // utility methods.
+      new Request('GET', 'updates/notifications').execute().then((r2) {
+        log('Notifications: ${r2["response"]["notifications"]["count"]}');
+      });
+
+      // Example of making a multi request consisting of three batched requests.
+      fsq.multi([fsq.users.get(), fsq.users.get('32'), fsq.users.get('33')])
+          .execute().then((r3) {
+            log('Multi responses: ${r3["response"]["responses"].length}');
+          });
     });
   });
+}
+
+void log(String s) {
+  Element e = document.$dom_createElement('pre');
+  e.text = s.toString();
+  document.body.elements.add(e);
 }
